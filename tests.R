8d450376ebeaec68d3e262e9fc1ec658ff1d6a54
erick <erick@autoweb.com>
2015-02-20 11:52:53 -0600

    Terminado el problema 2 son los metodos makeMatrix y cacheINverse, el metodo compare imprime tiempos de calculo

--- tests.R
+++ tests.R
@@ -25,6 +25,51 @@ cachemean <- function(x, ...) {
 }
 
 createRandomMatrix <- function() {
-  x <- matrix(rnorm(200), 5, 5)
+  x <- matrix(runif(1000000, min=0, max=100), 1000, 1000)
   x
+}
+
+createM <- function(){
+  matrix = matrix(c(4,2,7,6),nrow =2, ncol = 2)
+}
+
+makeMatrix <- function( x ){
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inverse) m <<- inverse
+    getInverse <- function() m
+    list(set = set, get = get,
+          setInverse = setInverse,
+          getInverse = getInverse)  
+}
+
+cacheInverse <- function(x, ...) {
+  m <- x$getInverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInverse(m)
+  m
+}
+
+compare <- function() {
+  matrix <- createRandomMatrix()
+
+  
+  ## Resultado de la funcion en cache
+  a <- makeMatrix(matrix)  
+  time<- system.time( cacheInverse(a) )
+  print(time)
+  
+  ## Resultado de Solve  
+  time<- system.time( solve(matrix) )
+  print(time)
+
 }
